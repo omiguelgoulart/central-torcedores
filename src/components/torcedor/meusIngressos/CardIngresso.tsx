cfc9b1060914c9ded3485858bc0f100552626a63
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2025-12-01 07:20:54 -0300

    :sparkles: feat: ajustar valor do plano conforme periodicidade selecionada

--- src/components/torcedor/meusIngressos/CardIngresso.tsx
+++ src/components/torcedor/meusIngressos/CardIngresso.tsx
@@ -44,7 +44,7 @@ export function CardIngresso({ ingresso }: CardIngressoProps) {
   return (
     <Card >
       <CardContent className="px-5 py-4">
-        <div className="flex items-center gap-4">
+        <div className="flex flex-row items-center gap-4">
           {/* Esquerda – jogo */}
           <div className="flex-1 min-w-0">
             <p className="mt-1 text-sm font-semibold text-foreground truncate">
@@ -56,24 +56,26 @@ export function CardIngresso({ ingresso }: CardIngressoProps) {
           </div>
 
           <div className="flex flex-col gap-2 items-centrer justify-center">
-            <Badge
-              className={` ${statusInfo.badgeClass} px-5` }
-            >
+            <div className="flex justify-end ">
+            <Badge className={` ${statusInfo.badgeClass}`}>
              {statusInfo.label}
             </Badge>
 
-            <div className="flex gap-2 text-xs text-muted-foreground">
-              <CalendarDays className="h-4 w-4" />
-              <span>Compra: {formatarDataHora(ingresso.criadoEm)}</span>
             </div>
-          </div>
+
+          
 
           <div className="flex flex-col items-end justify-between gap-2 min-w-[140px]">
-            <div className="text-right">
+            <div className="flex items-baseline gap-3">
               <p className="text-xs text-muted-foreground uppercase">Total</p>
               <p className="text-base font-semibold text-foreground">
                 {formatarValor(ingresso.valor)}
               </p>
+            </div>
+            <div className="flex gap-2 text-xs text-muted-foreground">
+              <CalendarDays className="h-4 w-4" />
+              <span>Compra: {formatarDataHora(ingresso.criadoEm)}</span>
+            </div>
             </div>
           </div>
         </div>
